d07dc097f22124c357faf47021b794ca545ed8ad
bylan1 <dthtb85@gmail.com>
2024-10-04 03:01:29 +0930

    changed number of buckets to be easily scalable

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ let ball;
 let dropped = false;
 let score = 0;
 let noBalls = 5;
+let noBuckets = 5;    // Must be an odd number (currently)
 
 // Standard setup and board preparation (pushing pegs and boundary lines)
 function setup() {
@@ -20,10 +21,10 @@ function draw() {
   }
   
   // Display bucket separators (8 score buckets of equal size)
-  for(let i=1; i<9; i++){
+  for(let i=1; i<noBuckets; i++){
     stroke(0);
     strokeWeight(2);
-    let lineX = i * (width / 9);
+    let lineX = i * (width / noBuckets);
     line(lineX, height - 25, lineX, height);
   }
 
@@ -62,25 +63,17 @@ function draw() {
   // Complete Plinko section, add to score and reset Plinko ball
   if(ball.getY() >= height){
     let ballX = ball.getX();
-    let scoreEdge = width / 9;
+    let scoreEdge = width / noBuckets;
     
-    // Switch statement adding score based on the landing bucket
-    switch(true){
-      case (ballX < scoreEdge || ballX > 8 * scoreEdge):
-        score += 10;
-        break;
-      case ((ballX > scoreEdge && ballX < 2 * scoreEdge) || (ballX > 7 * scoreEdge && ballX < 8 * scoreEdge)):
-        score += 20;
-        break;
-      case ((ballX > 2 * scoreEdge && ballX < 3 * scoreEdge) || (ballX > 6 * scoreEdge && ballX < 7 * scoreEdge)):
-        score += 40;
-        break;
-      case ((ballX > 3 * scoreEdge && ballX < 4 * scoreEdge) || (ballX > 5 * scoreEdge && ballX < 6 * scoreEdge)):
-        score += 80;
-        break;
-      case (ballX > 4 * scoreEdge && ballX < 5 * scoreEdge):
-        score += 160;
-        break;
+    // For loop adding score depending on which bucket landed in (within which y range)
+    for(let i=0; i<ceil(noBuckets/2); i++){
+      let lowerEdge1 = scoreEdge * i;
+      let lowerEdge2 = lowerEdge1 + scoreEdge;
+      let upperEdge1 = width - scoreEdge * i;
+      let upperEdge2 = upperEdge1 - scoreEdge;
+      if((ballX > lowerEdge1 && ballX < lowerEdge2) || (ballX > upperEdge2 && ballX < upperEdge1)){
+        score += (10 * (2**i));
+      }
     }
     
     // Set status to undropped and reset location of the Plinko ball
@@ -140,10 +133,11 @@ Your score: ${score}`
     textAlign(CENTER);
     textSize(15);
     fill([255, 0, 0]);
-    for(let i=0; i<5; i++){
-      text(`+${pow(2,i)*10}`, (2*width*i+width)/18, 775);
-      if(i<4){
-        text(`+${pow(2,i)*10}`, width - (2*width*i+width)/18, 775);
+    let halfBuckets = ceil(noBuckets/2)
+    for(let i=0; i<halfBuckets; i++){
+      text(`+${pow(2,i)*10}`, (2*width*i+width)/(noBuckets*2), 775);
+      if(i<halfBuckets-1){
+        text(`+${pow(2,i)*10}`, width - (2*width*i+width)/(noBuckets*2), 775);
       }
     }
   } else if (noBalls < 0){
@@ -167,14 +161,14 @@ function resetPlinko(ball){
 
 // Push ball elements (invisible separators, Plinko ball and board pegs) to their designated lists
 function prepareBoard(){
-  for(let i = 1; i < 9; i++){
-    ballBase.push(new Ball(i * (width / 9), height-25, 1, [0, 0, 0]));
-    ballBase.push(new Ball(i * (width / 9), height-12, 1, [0, 0, 0]));
+  for(let i = 1; i < noBuckets; i++){
+    ballBase.push(new Ball(i * (width / noBuckets), height-25, 1, [0, 0, 0]));
+    ballBase.push(new Ball(i * (width / noBuckets), height-12, 1, [0, 0, 0]));
   }
   
   ball = new PBall(width / 2, 50, 15, [255, 0, 0]);
   
-  let pegScale = 13;// Must be odd
+  let pegScale = 13;    // Must be odd
   let pegRad = 10;
   let levelSpace = 50;
   let startLevel = 150;
